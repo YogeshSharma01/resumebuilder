9d4e247dc7511aeb10db49699a8cc57b38478055
Aniket Wandile <pydeveloperaniket2001@gmail.com>
2023-11-01 11:08:15 +0530

    resume template update first

--- client/src/Components/About/About.js
+++ client/src/Components/About/About.js
@@ -1,84 +1,85 @@
 import React, { useEffect, useState } from 'react'
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 export default function About() {
 
   const navigate = useNavigate();
-  const [userData,setUserData] = useState(
+  const [ userData, setUserData ] = useState(
     {
-      name:"",
-      email:"",
-      phone:"",
-      address:"",
-      github:"",
-      linkdin:"",
-      portfolio:"",
-      education10:"",
-      education12:"",
-      education_graduation:"",
-      education_post_graduation:"",
-      skills:"",
-      project1:"",
-      about_project1:"",
-      project2:"",
-      about_project2:"",
-      declaration:""
+      name: "",
+      email: "",
+      phone: "",
+      address: "",
+      github: "",
+      linkdin: "",
+      portfolio: "",
+      education10: "",
+      education12: "",
+      education_graduation: "",
+      education_post_graduation: "",
+      skills: "",
+      project1: "",
+      about_project1: "",
+      project2: "",
+      about_project2: "",
+      declaration: ""
     });
-  const callAboutPage = async() =>{
-    try{
-      const res = await fetch('/about',{
-        method:'GET',
-        headers:{
-          Accept:"application/json",
-          'Content-Type':'application/json'
+  const callAboutPage = async () => {
+    try {
+      const res = await fetch('/about', {
+        method: 'GET',
+        headers: {
+          Accept: "application/json",
+          'Content-Type': 'application/json'
         },
-        credentials:"include"
+        credentials: "include"
       })
       const data = await res.json();
       console.log(data);
-      setUserData({...userData,
-        name:data.name,
-        email:data.email,
-        phone:data.phone,
+      setUserData({
+        ...userData,
+        name: data.name,
+        email: data.email,
+        phone: data.phone,
       });
-      if(!res.status === 200){
+      if (!res.status === 200) {
         const error = new Error(res.error);
         throw error;
       }
-    }catch(err){
+    } catch (err) {
       console.log(err);
       navigate('/login');
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     callAboutPage();
-  },[]);
+  }, []);
 
-  const handleInputs = (e) =>{
+  const handleInputs = (e) => {
     const name = e.target.name;
     const value = e.target.value;
 
-    setUserData({...userData,[name]:value})
+    setUserData({ ...userData, [ name ]: value })
   }
 
-  const sendResume = async(e) =>{
+  const sendResume = async (e) => {
     e.preventDefault();
-    const {name,email,phone,address,github,linkdin,portfolio,education10,education12,education_graduation,education_post_graduation,skills,project1,about_project1,project2,about_project2,declaration} = userData;
-    let res = await fetch('/resumedata',{
-      method:'POST',
-      headers:{
-        'Content-Type':'application/json'
+    const { name, email, phone, address, github, linkdin, portfolio, education10, education12, education_graduation, education_post_graduation, skills, project1, about_project1, project2, about_project2, declaration } = userData;
+    let res = await fetch('/resumedata', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
       },
-      body:JSON.stringify({
-        name,email,phone,address,github,linkdin,portfolio,education10,education12,education_graduation,education_post_graduation,skills,project1,about_project1,project2,about_project2,declaration
+      body: JSON.stringify({
+        name, email, phone, address, github, linkdin, portfolio, education10, education12, education_graduation, education_post_graduation, skills, project1, about_project1, project2, about_project2, declaration
       })
     });
     const data = res.json();
-    if(!data){
+    if (!data) {
       console.log("Resume not send!");
-    }else{
+    } else {
       window.alert("Resume Submit Successfully!");
     }
     navigate("/resume");
@@ -86,86 +87,87 @@ export default function About() {
 
   return (
     <>
-    <div className='container my-5 mx-5'>
-    <h1>Fill the form below and get your resume in minutes!</h1>
-      <div className='container my-5'>
-        <form method='POST'>
+      <div className='container my-5 mx-5'>
+        <h1>Fill the form below and get your resume in minutes!</h1>
+        <div className='container my-5'>
+          <form method='POST'>
             <h3>Contact Info</h3>
-              <div class="mb-3">
-                <label for="name" class="form-label">Name</label>
-                <input onChange={handleInputs} name="name" value={userData.name} type="text" class="form-control" id="name" aria-describedby="emailHelp" />
-              </div>
-              <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input onChange={handleInputs} name="email" value={userData.email} type="text" class="form-control" id="email" aria-describedby="emailHelp" />
-              </div>
-              <div class="mb-3">
-                <label for="phone" class="form-label">Phone</label>
-                <input onChange={handleInputs} name="phone" value={userData.phone} type="text" class="form-control" id="phone" aria-describedby="emailHelp" />
-              </div>
-              <div class="mb-3">
-                <label for="address" class="form-label">Address</label>
-                <input onChange={handleInputs} name="address" value={userData.address} type="text" class="form-control" id="address" aria-describedby="emailHelp" />
-              </div>
-              <div class="mb-3">
-                <label for="github" class="form-label">Github</label>
-                <input onChange={handleInputs} name="github" value={userData.github} type="text" class="form-control" id="github" aria-describedby="emailHelp" />
-              </div>
-              <div class="mb-3">
-                <label for="linkdin" class="form-label">Linkdin</label>
-                <input onChange={handleInputs} name="linkdin" value={userData.linkdin} type="text" class="form-control" id="linkdin" aria-describedby="emailHelp" />
-              </div>
-              <div class="mb-3">
-                <label for="portfolio" class="form-label">Portfolio</label>
-                <input onChange={handleInputs} name="portfolio" value={userData.portfolio} type="text" class="form-control" id="portfolio" aria-describedby="emailHelp" />
-              </div>
-              <h3>Education</h3>
-              <div class="mb-3">
-                <label for="10th" class="form-label">10th</label>
-                <input onChange={handleInputs} name="education10" value={userData.education10} type="text" class="form-control" id="10th" aria-describedby="emailHelp" />
-              </div>
-              <div class="mb-3">
-                <label for="12th" class="form-label">12th</label>
-                <input onChange={handleInputs} name="education12" value={userData.education12} type="text" class="form-control" id="12th" aria-describedby="emailHelp" />
-              </div>
-              <div class="mb-3">
-                <label for="graduation" class="form-label">Graduation</label>
-                <input onChange={handleInputs} name="education_graduation" value={userData.education_graduation} type="text" class="form-control" id="graduation" aria-describedby="emailHelp" />
-              </div>
-              <div class="mb-3">
-                <label for="post_graduation" class="form-label">Post Graduation</label>
-                <input onChange={handleInputs} name="education_post_graduation" value={userData.education_post_graduation} type="text" class="form-control" id="post_graduation" aria-describedby="emailHelp" />
-              </div>
-              <h3>Skills</h3>
-              <div class="mb-3">
-                <label for="skill" class="form-label">Skills</label>
-                <textarea rows={6} onChange={handleInputs} name="skills" value={userData.skills} type="text" class="form-control" id="skill" aria-describedby="emailHelp" />
-              </div>
-              <h3>Projects</h3>
-              <div class="mb-3">
-                <label for="project1" class="form-label">Project name 1</label>
-                <input onChange={handleInputs} name="project1" value={userData.project1} type="text" class="form-control" id="project1" aria-describedby="emailHelp" />
-              </div>
-              <div class="mb-3">
-                <label for="about_project1" class="form-label">About Project 1</label>
-                <textarea rows={6} onChange={handleInputs} name="about_project1" value={userData.about_project1} type="text" class="form-control" id="about_project1" aria-describedby="emailHelp" />
-              </div>
-              <div class="mb-3">
-                <label for="project2" class="form-label">Project name 2</label>
-                <input onChange={handleInputs} name="project2" value={userData.project2} type="text" class="form-control" id="project2" aria-describedby="emailHelp" />
-              </div>
-              <div class="mb-3">
-                <label for="about_project2" class="form-label">About Project 2</label>
-                <textarea rows={6} onChange={handleInputs} name="about_project2" value={userData.about_project2} type="text" class="form-control" id="about_project2" aria-describedby="emailHelp" />
-              </div>
-              <h3>Declaration</h3>
-              <label for="declaration" class="form-label">Declaration</label>
-                <textarea rows={6} onChange={handleInputs} name="declaration" value={userData.declaration} type="text" class="form-control" id="declaration" aria-describedby="emailHelp" />
+            <div class="mb-3">
+              <label for="name" class="form-label">Name</label>
+              <input onChange={handleInputs} name="name" value={userData.name} type="text" class="form-control" id="name" aria-describedby="emailHelp" />
+            </div>
+            <div class="mb-3">
+              <label for="email" class="form-label">Email</label>
+              <input onChange={handleInputs} name="email" value={userData.email} type="text" class="form-control" id="email" aria-describedby="emailHelp" />
+            </div>
+            <div class="mb-3">
+              <label for="phone" class="form-label">Phone</label>
+              <input onChange={handleInputs} name="phone" value={userData.phone} type="text" class="form-control" id="phone" aria-describedby="emailHelp" />
+            </div>
+            <div class="mb-3">
+              <label for="address" class="form-label">Address</label>
+              <input onChange={handleInputs} name="address" value={userData.address} type="text" class="form-control" id="address" aria-describedby="emailHelp" />
+            </div>
+            <div class="mb-3">
+              <label for="github" class="form-label">Github</label>
+              <input onChange={handleInputs} name="github" value={userData.github} type="text" class="form-control" id="github" aria-describedby="emailHelp" />
+            </div>
+            <div class="mb-3">
+              <label for="linkdin" class="form-label">Linkdin</label>
+              <input onChange={handleInputs} name="linkdin" value={userData.linkdin} type="text" class="form-control" id="linkdin" aria-describedby="emailHelp" />
+            </div>
+            <div class="mb-3">
+              <label for="portfolio" class="form-label">Portfolio</label>
+              <input onChange={handleInputs} name="portfolio" value={userData.portfolio} type="text" class="form-control" id="portfolio" aria-describedby="emailHelp" />
+            </div>
+            <h3>Education</h3>
+            <div class="mb-3">
+              <label for="10th" class="form-label">10th</label>
+              <input onChange={handleInputs} name="education10" value={userData.education10} type="text" class="form-control" id="10th" aria-describedby="emailHelp" />
+            </div>
+            <div class="mb-3">
+              <label for="12th" class="form-label">12th</label>
+              <input onChange={handleInputs} name="education12" value={userData.education12} type="text" class="form-control" id="12th" aria-describedby="emailHelp" />
+            </div>
+            <div class="mb-3">
+              <label for="graduation" class="form-label">Graduation</label>
+              <input onChange={handleInputs} name="education_graduation" value={userData.education_graduation} type="text" class="form-control" id="graduation" aria-describedby="emailHelp" />
+            </div>
+            <div class="mb-3">
+              <label for="post_graduation" class="form-label">Post Graduation</label>
+              <input onChange={handleInputs} name="education_post_graduation" value={userData.education_post_graduation} type="text" class="form-control" id="post_graduation" aria-describedby="emailHelp" />
+            </div>
+            <h3>Skills</h3>
+            <div class="mb-3">
+              <label for="skill" class="form-label">Skills</label>
+              <textarea rows={6} onChange={handleInputs} name="skills" value={userData.skills} type="text" class="form-control" id="skill" aria-describedby="emailHelp" />
+            </div>
+            <h3>Projects</h3>
+            <div class="mb-3">
+              <label for="project1" class="form-label">Project name 1</label>
+              <input onChange={handleInputs} name="project1" value={userData.project1} type="text" class="form-control" id="project1" aria-describedby="emailHelp" />
+            </div>
+            <div class="mb-3">
+              <label for="about_project1" class="form-label">About Project 1</label>
+              <textarea rows={6} onChange={handleInputs} name="about_project1" value={userData.about_project1} type="text" class="form-control" id="about_project1" aria-describedby="emailHelp" />
+            </div>
+            <div class="mb-3">
+              <label for="project2" class="form-label">Project name 2</label>
+              <input onChange={handleInputs} name="project2" value={userData.project2} type="text" class="form-control" id="project2" aria-describedby="emailHelp" />
+            </div>
+            <div class="mb-3">
+              <label for="about_project2" class="form-label">About Project 2</label>
+              <textarea rows={6} onChange={handleInputs} name="about_project2" value={userData.about_project2} type="text" class="form-control" id="about_project2" aria-describedby="emailHelp" />
+            </div>
 
-                <br/>
-                <button onClick={sendResume} className='btn btn-outline-success'>Submit</button>
-        </form>
-      </div>
+            <h3>Declaration</h3>
+            <label for="declaration" class="form-label">Declaration</label>
+            <textarea rows={6} onChange={handleInputs} name="declaration" value={userData.declaration} type="text" class="form-control" id="declaration" aria-describedby="emailHelp" />
+
+            <br />
+            <button onClick={sendResume} className='btn btn-outline-success'>Submit</button>
+          </form>
+        </div>
       </div>
     </>
   )
--- client/src/Components/Resume/Resume.js
+++ client/src/Components/Resume/Resume.js
@@ -1,49 +1,175 @@
-import React,{useEffect,useState} from 'react';
-// import './Resume.css';
+import React, { useEffect, useState } from 'react';
+import './Resume.css';
 
 export default function Resume() {
 
-    const [userData,setUserData] = useState('');
-    const callContact = async() =>{
-      try{
-        const res = await fetch('/getdata',{
-          method:'GET',
-          headers:{
-            'Content-Type':'application/json'
-          },
-        })
-        const data = await res.json();
-        console.log(data);
-        setUserData(data);
-        if(!res.status === 200){
-          const error = new Error(res.error);
-          throw error;
-        }
-      }catch(err){
-        console.log(err);
+  const [ userData, setUserData ] = useState('');
+  const callContact = async () => {
+    try {
+      const res = await fetch('/getdata', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+      const data = await res.json();
+      console.log(data);
+      setUserData(data);
+      if (!res.status === 200) {
+        const error = new Error(res.error);
+        throw error;
       }
+    } catch (err) {
+      console.log(err);
     }
-  
-    useEffect(()=>{
-      callContact();
-    },[]);
+  }
 
-    // Conditional rendering to check if userData and userData.resumedata are available
-    if (!userData || !userData.resumedata || userData.resumedata.length === 0) {
-      // Show loading state or return null
-      return <div>Loading...</div>;
+  useEffect(() => {
+    callContact();
+  }, []);
+
+  const handlePrint = () => window.print();
+
+  // Conditional rendering to check if userData and userData.resumedata are available
+  if (!userData || !userData.resumedata || userData.resumedata.length === 0) {
+    // Show loading state or return null
+    return <div>Loading...</div>;
   }
 
   return (
-    <div className="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
-      <div className="col-lg-6 px-0">
-        <h1 className="display-4 fst-italic">{userData.name}</h1>
-        <p className="lead my-3">{userData.resumedata[0].address}</p>
-        <p className="lead my-3">{userData.resumedata[0].phone}</p>
-        <p className="lead my-3">{userData.resumedata[0].portfolio}</p>
-        <a href={userData.resumedata[0].github}><p className="lead my-3">{userData.resumedata[0].github}</p></a>
-        <a href={userData.resumedata[0].linkdin}><p className="lead my-3">{userData.resumedata[0].linkdin}</p></a>
+    <div>
+      <meta charSet="UTF-8" />
+      <title>Resume | Template</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/paper-css/0.4.1/paper.min.css" />
+      <link rel="stylesheet" href="style.css" />
+      <div className="sheet">
+        <button className="btn btn-print btn-sm btn-light" onClick={handlePrint}>
+          <i className="fa fa-print" />
+          Print
+        </button>
+        <div className="two-column resume">
+
+          <section className="resume__section resume__header">
+            <div className="resume__content">
+              <h1>{userData.name}</h1>
+              <div className="info-item"><span className="info-label"><i className="fa fa-location-arrow" /></span><span className="info-text">
+                {userData.resumedata[ 0 ].address}</span></div>
+              <div className="info-item"><span className="info-label"><i className="fa fa-envelope" /></span><span className="info-text"><a href="/cdn-cgi/l/email-protection" className="__cf_email__" data-cfemail="f1929e9f8590859eb185999890969e9383909690df9e8396">{userData.resumedata[ 0 ].email}</a></span></div>
+              <div className="info-item"><span className="info-label"><i className="fa fa-phone" /></span><span className="info-text">{userData.resumedata[ 0 ].phone}</span></div>
+              <div className="info-item"><span className="info-label"><i className="fa fa-linkedin" /></span><span className="info-text"><a href="/cdn-cgi/l/email-protection" className="__cf_email__" data-cfemail="f1929e9f8590859eb185999890969e9383909690df9e8396">{userData.resumedata[ 0 ].linkdin}</a></span></div>
+              <div className="info-item"><span className="info-label"><i className="fa fa-globe" /></span><span className="info-text"><a href={userData.resumedata[ 0 ].portfolio} target="_blank" rel="noopener noreferrer">
+                {userData.resumedata[ 0 ].portfolio}
+              </a></span></div>
+            </div>
+          </section>
+
+          <div className="resume__columns">
+            <div className="resume__main">
+              <section className="resume__section resume__summary">
+                <div className="resume__content">
+                  <div className="resume__section-title"><i className="fa fa-pencil-square-o" />
+                    <h2>Professional Summary</h2>
+                  </div>
+                  <div className="other">
+                    <div className="other-info">
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, mollitia! Ab magni eligendi consequuntur, in aliquid reprehenderit a ratione optio et doloremque doloribus exercitationem voluptatem unde, laudantium ad ipsum at aspernatur voluptates sed accusantium numquam soluta vero delectus. Mollitia, illum.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <section className="resume__section resume__experience">
+                <div className="resume__content">
+                  <div className="resume__section-title"><i className="fa fa-briefcase" />
+                    <h2>Project</h2>
+                  </div>
+                  <div className="xp-item">
+                    <div className="xp-job">
+                      {userData.resumedata[ 0 ].project1}
+                    </div>
+                    <div className="xp-date">Apr. 2017 – current</div>
+                    <div className="xp-detail">
+                      <ul>
+                        <li>
+                          {userData.resumedata[ 0 ].about_project1}
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="xp-item">
+                    <div className="xp-job">
+                      {userData.resumedata[ 0 ].project2}
+                    </div>
+                    <div className="xp-date">Aug. 2018 – Apr. 2020</div>
+                    <div className="xp-detail">
+                      <ul>
+                        <li>
+                          {userData.resumedata[ 0 ].about_project2}
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+            <div className="resume__side">
+              <section className="resume__section resume__skills">
+                <div className="resume__content">
+                  <div className="resume__section-title"><i className="fa fa-align-center" />
+                    <h2>Skills</h2>
+                  </div>
+                  <div className="resume__text">               
+                    <div className="extra">
+                      <div className="extra-info">{userData.resumedata[ 0 ].skills}</div>
+                      <div className="extra-details">
+                        <div className="extra-details__progress" style={{ width: '90%' }} />
+                      </div>
+                    </div>
+                    <div className="extra">
+                      <div className="extra-info">Operating Systems<br /><small>
+                        <i className="fa fa-linux" /> GNU/Linux ·
+                        <i className="fa fa-apple" /> Mac OS ·
+                        <i className="fa fa-windows" /> Windows</small></div>
+                      <div className="extra-details">
+                        <div className="extra-details__progress" style={{ width: '90%' }} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <section className="resume__section resume__languages">
+                <div className="resume__content">
+                  <div className="resume__section-title"><i className="fa fa-globe" />
+                    <h2>Languages</h2>
+                  </div>
+                  <div className="extra">
+                    <div className="extra-info">Portuguese <small>(native)</small></div>
+                    <div className="extra-details">
+                      <div className="extra-details__progress" style={{ width: '100%' }} />
+                    </div>
+                  </div>
+                  <div className="extra">
+                    <div className="extra-info">English</div>
+                    <div className="extra-details">
+                      <div className="extra-details__progress" style={{ width: '65%' }} />
+                    </div>
+                  </div>
+                  <div className="extra">
+                    <div className="extra-info">Spanish</div>
+                    <div className="extra-details">
+                      <div className="extra-details__progress" style={{ width: '20%' }} />
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+
   )
 }
